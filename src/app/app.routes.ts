07b3e548da6e8ea0c7703ad66aa4ed0402f6b0be
saloum45 <saloumfall45@gmail.com>
2023-12-29 12:08:13 +0000

    in progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"header",
+    pathMatch:"full"
+  },
+  {
+    path:"header",
+    component:HeaderComponent
+  }
+];
