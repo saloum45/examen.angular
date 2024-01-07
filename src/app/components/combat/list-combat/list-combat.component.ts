d017c365060738553623f4beec84d5dfc8bdf056
saloum45 <saloumfall45@gmail.com>
2024-01-07 18:56:26 +0000

    in progress

--- src/app/components/combat/list-combat/list-combat.component.ts
+++ src/app/components/combat/list-combat/list-combat.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../service/api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-combat',
   standalone: true,
   imports: [],
   templateUrl: './list-combat.component.html',
-  styleUrl: './list-combat.component.css'
+  styleUrl: './list-combat.component.css',
+  providers: [
+    ApiService,
+    HttpClient
+  ]
 })
-export class ListCombatComponent {
+export class ListCombatComponent implements OnInit {
+  // Attributs
+  public combats: any[] = [];
 
+
+  // Methodes
+  constructor(private api: ApiService) {
+
+  }
+
+  ngOnInit(): void {
+    this.api.taf_get("combat/get_form_details", (reponse: any) => {
+      // this.commbats = reponse.data.les_ecuries_lutteurs;
+      console.warn("combats", reponse);
+      this.combats=reponse.data.combat;
+      console.warn("combats tb",this.combats );
+    }, (error: any) => {
+      console.log(error);
+    });
+  }
 }
--- src/app/components/lutteur/list-lutteur/list-lutteur.component.ts
+++ src/app/components/lutteur/list-lutteur/list-lutteur.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../service/api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-lutteur',
   standalone: true,
   imports: [],
   templateUrl: './list-lutteur.component.html',
-  styleUrl: './list-lutteur.component.css'
+  styleUrl: './list-lutteur.component.css',
+  providers:[
+    ApiService,
+    HttpClient
+  ]
 })
-export class ListLutteurComponent {
+export class ListLutteurComponent implements OnInit{
+  // Attributs
+  // public lutteurs:any[]=[];
+  // public ecuries:any[]=[];
+  public lutteursEcuries:any[]=[];
 
+
+  // Methodes
+  constructor(private api:ApiService){
+
+  }
+
+  ngOnInit(): void {
+    this.api.taf_get("lutteur/get_form_details",(reponse:any)=>{
+      this.lutteursEcuries=reponse.data.les_ecuries_lutteurs;
+          console.warn("repon",reponse);
+        },(error:any)=>{
+          console.log(error);
+        });
+  }
 }
