f0f2097afdf34c37f5bd3e5b275a72464af6c55c
saloum45 <saloumfall45@gmail.com>
2024-01-09 11:05:39 +0000

    in progress

--- src/app/components/combat/add-combat/add-combat.component.ts
+++ src/app/components/combat/add-combat/add-combat.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../../service/api.service';
+import { SweetMessageService } from '../../../service/sweet-message.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-combat',
   standalone: true,
   imports: [],
   templateUrl: './add-combat.component.html',
-  styleUrl: './add-combat.component.css'
+  styleUrl: './add-combat.component.css',
+  providers:[
+    ApiService,
+    SweetMessageService
+  ]
 })
 export class AddCombatComponent {
+ // Attributs
+ public nom = "";
+ public taille = "";
+ public poids = "";
+ public photo = "";
+ public description = "";
+ public age = "";
+ public id_ecurie = "";
+ public ecuries: any = [];
 
+
+ // Methodes
+ constructor(private api: ApiService, private message: SweetMessageService, private router: Router) {
+
+ }
+ ngOnInit(): void {
+   this.api.taf_get("ecurie/get", (reponse: any) => {
+     if (reponse.status) {
+       this.ecuries = reponse.data;
+     }
+   }, (error: any) => {
+     console.log(error);
+   })
+ }
+ Ajouter() {
+   if (this.nom == "" || this.age == "" || this.photo == "" || this.poids == "" || this.taille == "" || this.description == "") {
+     this.message.simpleMessage("error", "oop's", "Veuillez renseigner tous les champs");
+   } else {
+     this.api.taf_post("lutteur/add", { nom: this.nom, age: this.age, photo: this.photo, poids: this.poids, taille: this.taille, description: this.description,id_ecurie:this.id_ecurie }, (reponse: any) => {
+       if (reponse.status) {
+
+         this.message.simpleMessage("success", "Merci", "Ajout fait avec succès");
+         this.router.navigate(['/listLutteurs']);
+       }
+     }, (error: any) => {
+       console.log(error);
+     })
+   }
+ }
 }
